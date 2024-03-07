eeb9f325ec148dca33af1be60a9c3c743d25360e
diogodacunhanunes <124520161+diogodacunhanunes@users.noreply.github.com>
2024-03-07 09:41:39 +0100

    trying to add url

--- shop/src/app/main/page.tsx
+++ shop/src/app/main/page.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import MainPage from "../Components/Main/MainPage";
 
 export default async function Main() {
-  const productsResp = await fetch(
-    `${process.env.NEXTAUTH_URL}/api/products/main`,
-    {
-      headers: {
-        Accept: "application/json",
-        method: "GET",
-      },
-    }
-  );
+  const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000/"; // Default to local URL if NEXTAUTH_URL is not set
+  const endpoint = "/api/products/main";
+
+  const url = `${baseUrl.replace(/\/$/, "")}${endpoint}`;
+
+  console.log("url", url);
+
+  const productsResp = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
 
   const products = await productsResp.json();
 
